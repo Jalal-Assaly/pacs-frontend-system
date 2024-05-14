606dc8d797e2d3e644afc91bd4ec5b64a3d5def5
AhmedShalabi01 <119356507+AhmedShalabi01@users.noreply.github.com>
2024-05-15 00:17:42 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'user-credentials', loadChildren: () => import('./user-credentials/user-credentials.module').then(m => m.UserCredentialsModule) }
+  { path: 'attributes-management', loadChildren: () => import('./attributes-management/attributes-management.module').then(m => m.AttributesManagementModule) }
 ];
 
 @NgModule({
